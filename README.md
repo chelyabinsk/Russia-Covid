d614f2e2bb32dcfe082b4647e874bf87dedc84b0
Chelyabinsk <15384813+chelyabinsk@users.noreply.github.com>
2021-12-23 20:16:21 +0000

    test

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Russia-Covid
+# Official Russia COVID-19 statistics [https://стопкоронавирус.рф]
+Russia publishes their daily COVID-19 statistics, however there is no easy way to download it. This repository stores daily snapshots of their data.
+Latest data is saved in a .CSV table, as well as the raw snapshots in JSON format.
--- main.py
+++ main.py
@@ -8,23 +8,86 @@ import json
 import re
 import os
 import datetime
+import random
 
 url = "https://стопкоронавирус.рф/information/"
-
-webpage_html = requests.get(url)
-region_codes = re.findall('data-code=\"(.*?)\"',webpage_html.text)
-
-data_base_url = "https://xn--80aesfpebagmfblc0a.xn--p1ai/covid_data.json?do=region_stats&code={}"
-
+data_base_url_format = "https://xn--80aesfpebagmfblc0a.xn--p1ai/covid_data.json?do=region_stats&code={}"
 datetime_str = datetime.datetime.now().strftime("%Y-%m-%d")
 
-for code in region_codes:
+webpage_html = requests.get(url)
+#region_codes = re.findall('data-code=\"(.*?)\"',webpage_html.text)
+
+# Dictionary of names
+with open("full/regions name map.json", "r", encoding="utf8") as f:
+    region_name_dictionary = json.loads(f.read())
+
+# Extract today's json from html
+today_json = json.loads(re.findall("spread-data='(.*?)'",webpage_html.text)[0])
+for region in today_json:
+    code = region["code"]
     directory = "regions/{}/{}".format(code,datetime_str)
-    data_base_url = data_base_url.format(code)
     if not os.path.exists(directory):
         os.makedirs(directory)
+    with open(directory + "/vaccines.json", "w", encoding="utf8") as f:
+        f.write(json.dumps(region))
+        
+# Write full table
+if not os.path.exists("full/vaccines.csv"):
+    header_row = "Folder name, Region name Russian, Region name English, Date, Extract Date, Infected, Recovered, Deceased, First jab, Second jab, Immunity pct\n"
+    with open("full/vaccines.csv","w",encoding="utf8") as f:
+        f.write(header_row)
+with open("full/vaccines.csv","a",encoding="utf8") as f:
+    for region in today_json:
+        row_text = "\t".join(
+            str(i) for i in [
+                region["code"],
+                region_name_dictionary[region["code"]]["ru"],
+                region_name_dictionary[region["code"]]["en"],
+                region["date"],
+                datetime_str,
+                region["sick"],
+                region["healed"],
+                region["died"],
+                region["first"],
+                region["second"],
+                float(region["immune_percent"]),
+                ]
+            ) + "\n"
+        f.write(row_text)
+
+
+
+region_codes = list(region_name_dictionary.keys())
+
+
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+
+json_list = []
+# Download and save raw JSON files
+for code in region_name_dictionary.keys():
+    directory = "regions/{}/{}".format(code,datetime_str)
+    data_base_url = data_base_url_format.format(code)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    useragent = "".join([alphabet[random.randint(0,len(alphabet)-1)] for i in range(random.randint(0,36))])
+    headers = {"User-Agent": useragent}
     print(code)
-    json_response = requests.get(data_base_url).json()
+    json_response = requests.get(data_base_url,headers=headers).json()
+    json_list.append(json_response)
     
     with open(directory + "/data.json","w") as f:
         f.write(json.dumps(json_response))
+        
+
+
+# Process tables
+with open("full/infections.csv", "w") as f:
+    # Write header
+    f.write("Folder name, Region name Russian, Region name English, Date, Infected, Recovered, Deceased\n")
+    for region_count, region_data in enumerate(json_list):
+        for line_count,line in enumerate(region_data):
+            line_data = ",".join(str(i) for i in [region_codes[region_count],
+                  region_name_dictionary[region_codes[region_count]]["ru"],
+                  region_name_dictionary[region_codes[region_count]]["en"],
+                  line["date"],line["sick"],line["healed"],line["died"]]) + "\n"
+            f.write(line_data)
